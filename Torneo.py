95be6d2e9c91f34e23c895f9ce059cc68b1f8124
Karime-Mtz <A01709690@tec.mx>
2025-10-15 07:33:15 -0600

    Avance 6
    Listas

--- Torneo.py
+++ Torneo.py
@@ -1,122 +1,98 @@
 #Creación de listas:
-participantes_mujeres = []
-participantes_hombres = []
-ligero_m = []
-ligero_h = []
-welter_m = []
-welter_h = []
-pesado_m = []
-pesado_h = []
-i = 0
-capacidad_max = 15
+participantes = {
+    "mujer": {"ligero": [], "welter": [], "pesado": []},
+    "hombre": {"ligero": [], "welter": [], "pesado": []}
+}
 
 
 #Agregar elementos a listas:
 #Operadores: ==, >=, <=, and
-
-while i <= capacidad_max:
-    def datos_participante():
-        nombre = input("¿Cuál es tu nombre? ")
+def datos_participante():
+    nombre = input("¿Cuál es tu nombre? ")
         
-        sexo = input("¿Cuál es tu sexo? (mujer/hombre) ").lower()
-        if sexo == "mujer":
-          participantes_mujeres.append(nombre)
-        elif sexo == "hombre":
-          participantes_hombres.append(nombre)
-        else:
-            while sexo != "mujer" and sexo != "hombre":
-                print("Respuesta no válida")
-                sexo = input("¿Cuál es tu sexo? (mujer/hombre) ").lower() 
+    sexo = input("¿Cuál es tu sexo? (mujer/hombre) ").lower()
+    while sexo != "mujer" and sexo != "hombre":
+        print("Respuesta no válida")
+        sexo = input("¿Cuál es tu sexo? (mujer/hombre) ").lower() 
         
-        peso = float(input("¿Cuál es tu peso? "))
-        return nombre, sexo, peso
+    peso = float(input("¿Cuál es tu peso? "))
 
-    nombre, sexo, peso = datos_participante()
-    #print(nombre, sexo, peso)
+    if peso < 68:
+        categoria = "ligero"
+    elif peso >= 68 and peso < 80:
+        categoria = "welter"
+    elif peso >= 80:
+        categoria = "pesado"
 
-    if sexo == "mujer": 
-      if peso < 68:
-        ligero_m.append(nombre)
-      elif peso >= 68 and peso < 80:
-        welter_m.append(nombre)
-      elif peso >= 80:
-        pesado_m.append(nombre)
-
-    elif sexo == "hombre":
-      if peso < 68:
-        ligero_h.append(nombre)
-      elif peso >= 68 and peso < 80:
-        welter_h.append(nombre)
-      elif peso >= 80:
-        pesado_h.append(nombre)
+    participantes[sexo][categoria].append(nombre)
+    return nombre, sexo, peso
     
+i = 0
+capacidad_max = 15   
+while i <= capacidad_max:
+    datos_participante()
     i = i+1
     
-    
-print("lista de mujeres", participantes_mujeres)
-print("lista de hombres", participantes_hombres)
-print("categoria ligero mujeres", ligero_m)
-print("categoria ligero hombres", ligero_h)
-print("categoria welter mujeres", welter_m)
-print("categoria welter hombres", welter_h)
-print("categoria pesado mujeres", pesado_m)
-print("categoria pesado hombres", pesado_h)
+print("categoria ligero mujeres", participantes["mujer"]["ligero"])
+print("categoria ligero hombres", participantes["hombre"]["ligero"])
+print("categoria welter mujeres", participantes["mujer"]["welter"])
+print("categoria welter hombres", participantes["hombre"]["welter"])
+print("categoria pesado mujeres", participantes["mujer"]["pesado"])
+print("categoria pesado hombres", participantes["hombre"]["pesado"])
 
 #Cantidad de elementos en las listas
 #operador de suma
 
 #Funciones para calcular cantidad de participantes
-
 def cantidad_mujeres():
-  num_mujeres = len(participantes_mujeres)
-  return num_mujeres
+    acum = 0
+    for a in participantes["mujer"]:
+        acum = acum +len(participantes["mujer"][a])
+    return acum
 
 def cantidad_hombres():
-  num_hombres = len(participantes_hombres)
-  return num_hombres
+    acum = 0
+    for a in participantes["hombre"]:
+        acum = acum +len(participantes["hombre"][a])
+    return acum
 
 def cantidad_participantes():
-  num_participantes = cantidad_mujeres() + cantidad_hombres()
-  return num_participantes
-
+  return cantidad_mujeres()+cantidad_hombres()
 
 #Funciones para calcular cantidad de personas en cada categoría
 
 def cantidad_ligero_m():
-  num_ligero_m = len(ligero_m)
-  return num_ligero_m
+    cant = len(participantes["mujer"]["ligero"])
+    return cant
 
 def cantidad_ligero_h():
-  num_ligero_h = len(ligero_h)
-  return num_ligero_h
+    cant = len(participantes["hombre"]["ligero"])
+    return cant
 
 def cantidad_ligero_total():
-  num_ligero_total = cantidad_ligero_m() + cantidad_ligero_h()
-  return num_ligero_total
+  return cantidad_ligero_m() + cantidad_ligero_h()
 
 def cantidad_welter_m():
-  num_welter_m = len(welter_m)
-  return num_welter_m
+  cant = len(participantes["mujer"]["welter"])
+  return cant
 
 def cantidad_welter_h():
-  num_welter_h = len(welter_h)
-  return num_welter_h
+  cant = len(participantes["hombre"]["welter"])
+  return cant
 
 def cantidad_welter_total():
-  num_welter_total = cantidad_welter_m() + cantidad_welter_h()
-  return num_welter_total
+  return cantidad_welter_m() + cantidad_welter_h()
 
 def cantidad_pesado_m():
-  num_pesado_m = len(pesado_m)
-  return num_pesado_m
+  cant = len(participantes["mujer"]["pesado"])
+  return cant
 
 def cantidad_pesado_h():
-  num_pesado_h = len(pesado_h)
-  return num_pesado_h
+  cant = len(participantes["hombre"]["pesado"])
+  return cant
 
 def cantidad_pesado_total():
-  num_pesado_total = cantidad_pesado_m() + cantidad_pesado_h()
-  return num_pesado_total
+  return cantidad_pesado_m() + cantidad_pesado_h()
 
 #Funciones para calcular número de combates
 
@@ -152,6 +128,7 @@ def combates_hombres():
 def combates_totales():
   num_combates_total = combates_mujeres() + combates_hombres()
   return num_combates_total
+
 
 #Funciones para calcular el porcentaje de mujeres y hombres en la competencia
 
